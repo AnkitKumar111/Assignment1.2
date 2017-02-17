eb53fcc01327e4ecc2cf863e0bb7cc4b70ce0ca7
AnkitKumar111 <ankitwelcomesyou@gmail.com>
2017-02-16 16:25:34 -0800

    Assignment1.2
    program to print the result of the expressions

---
+++ src/exp_result.java
@@ -0,0 +1,24 @@
+
+public class exp_result {
+		
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=10;
+		
+		// arithmetic using integers
+				System.out.println("Integer Arithmetic");
+				int x=a-- - --a;
+			        b=x;
+				int c=a--;
+				int d=a>>2;
+				int e=a&b;
+
+				System.out.println("b = " + b);
+				System.out.println("c = " + c);
+				System.out.println("d = " + d);
+				System.out.println("e = " + e);
+				
+	}
+
+}
